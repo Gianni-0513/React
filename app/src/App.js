52605e8367b211bdf1b53d9f93885b5fbb2e85ae
lai2-24 <lai2-24@itismeucci.com>
2025-04-29 14:37:21 +0200

    Gianni

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyButton from './MyButton.js'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+
+}
+
+export default App;
